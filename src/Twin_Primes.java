cf48ffb5d2fb0344d7760368deb924e476295107
Gideon Tettehfio <gideonlambride@gmail.com>
2022-04-07 10:32:18 +0000

    Twin prime numbers

--- src/Twin_Primes.java
+++ src/Twin_Primes.java
@@ -1,25 +1,26 @@
 import java.util.Scanner;
+
 public class Twin_Primes {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int lastnum, factors =0;
-        System.out.println("Enter the last number::");
-        lastnum = input.nextInt();
-        int [] primes = new int[lastnum];
-        for (int i=2; i<=lastnum; i++) {
-            for (int j=2; j<i; j++) {
-                 if(i%j == 0) {
-                     factors ++;
-                 }
-            }
-            if (factors ==0) {
-                for (int k=0; k<lastnum; k++) {
-                    primes[k] = i;
-                }
+    private static boolean isPrime(int a) {
+        int factors=0;
+        for (int i = 2; i < a; i++) {
+            if (a % i == 0) {
+                factors++;
             }
         }
-        for (int x=0; x<lastnum; x++) {
-            System.out.print(primes[x]);
+        return factors==0;
+    }
+    public static void main (String[]args){
+        Scanner input = new Scanner(System.in);
+        int lastnum;
+        System.out.println("Enter the last number::");
+        lastnum = input.nextInt();
+        for (int i=2; i<=lastnum; i++) {
+            if (isPrime(i) && isPrime((i+2))) {
+                System.out.println(i + " and " + (i+2));
+
+            }
         }
     }
 }
+
